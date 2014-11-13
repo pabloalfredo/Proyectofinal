81f60a1d87a24456ec822bf458d9ca53e3a3a4a1
pabloalfredo <pabloalfredo@pabloalfredo-PC>
2014-11-13 13:34:21 -0800

    menu

--- ProyectoFinal/src/Formularios/frmPrincipal.java
+++ ProyectoFinal/src/Formularios/frmPrincipal.java
@@ -3,6 +3,13 @@ package Formularios;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JDesktopPane;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JToolBar;
 
 public class frmPrincipal {
 
@@ -39,6 +46,23 @@ public class frmPrincipal {
 		frmFormularioPrincipal.setTitle("Formulario Principal");
 		frmFormularioPrincipal.setBounds(100, 100, 450, 300);
 		frmFormularioPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JDesktopPane desktopPane = new JDesktopPane();
+		desktopPane.setBackground(Color.LIGHT_GRAY);
+		frmFormularioPrincipal.getContentPane().add(desktopPane, BorderLayout.CENTER);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBounds(0, 0, 434, 23);
+		desktopPane.add(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("New menu");
+		menuBar.add(mnNewMenu);
+		
+		JMenu mnNewMenu_1 = new JMenu("New menu");
+		menuBar.add(mnNewMenu_1);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("New menu item");
+		mnNewMenu_1.add(mntmNewMenuItem);
+		
 	}
-
 }
