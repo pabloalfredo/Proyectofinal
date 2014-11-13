f645031bed40e9f4f171e2ec2ffdbf6d4c729a05
vladimir <vladimir@vladimir-PC>
2014-11-13 18:08:17 -0400

    Creacion de clase mtdTipoProducto y cambios en la conexion y Clase tipoProducto

--- ProyectoFinal/src/Clases/BaseDeDatos.java
+++ ProyectoFinal/src/Clases/BaseDeDatos.java
@@ -1,5 +1,44 @@
 package Clases;
 
-public class BaseDeDatos {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+
+
+public class BaseDeDatos {
+	private Connection cn;
+    private static String URL ="jdbc:mysql://localhost/dbprueba1";
+    private static String USUARIO = "root";
+    private static String PASSWORD ="curne00";
+    
+    public void  open(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            cn = DriverManager.getConnection(URL, USUARIO, PASSWORD);        
+        }catch(Exception ex){
+            Logger.getLogger(BaseDeDatos.class.getName()).log(Level.SEVERE,null,ex);
+        }
+    }
+    public ResultSet query (String sql){
+        ResultSet rs;
+        try{
+            rs =cn.createStatement().executeQuery(sql);
+            return rs;
+        }catch(SQLException e){}
+            return null;
+    }
+    
+    public void execute (String sql){
+        try{
+            cn.createStatement().execute(sql);
+       }catch(SQLException e){}
+    }
+    
+    public void close (){
+        cn=null;
+    }
 }
