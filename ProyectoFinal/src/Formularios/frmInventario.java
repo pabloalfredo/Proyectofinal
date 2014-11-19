27801490e37b0ba13aded6360a8c28a1e0c158d3
vladimir <vladimir@vladimir-PC>
2014-11-18 22:30:05 -0400

    cambios

--- ProyectoFinal/src/Formularios/frmInventario.java
+++ ProyectoFinal/src/Formularios/frmInventario.java
@@ -32,7 +32,7 @@ public class frmInventario extends JFrame {
 	 */
 	public frmInventario() {
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 459, 312);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
