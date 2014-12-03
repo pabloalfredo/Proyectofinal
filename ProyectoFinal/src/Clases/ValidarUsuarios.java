99e49dae756f27722b0ac63818f648910da8b3a8
pabloalfredo <pabloalfredo@pabloalfredo-PC>
2014-12-03 14:29:10 -0800

    clase validar usuario

---
+++ ProyectoFinal/src/Clases/ValidarUsuarios.java
@@ -0,0 +1,37 @@
+package Clases;
+
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import Clases.BaseDeDatos;
+
+public class ValidarUsuarios {
+	
+    public  boolean validarUsuario(String usuario, String contrasena)  throws IOException
+    {
+        try
+        {
+            
+   
+        	
+        	BaseDeDatos conectar = new BaseDeDatos();
+        	String sql ="SELECT * FROM tblusuario WHERE usuario='"+usuario+"' AND clave='"+ contrasena+"'";
+        	Statement instruccionSQL = conectar.getConexion().createStatement();
+        	ResultSet resultadoConsulta = instruccionSQL.executeQuery(sql);
+        	
+ 
+            if( resultadoConsulta.first() )        // si es valido el primer reg. hay una fila, tons el usuario y su pw existen
+                return true;        //usuario validado correctamente
+            else
+                return false;        //usuario validado incorrectamente
+                 
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+ 
+    }
+
+}
